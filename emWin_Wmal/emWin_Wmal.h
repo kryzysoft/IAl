bea1a0e084d5b7c31e2cc262ba92e211c5fa96e2
kryzysoft <kryzysoft@gmail.com>
2019-12-27 23:50:45 +0100

    Warnings on CubeIDE and CodeBlocks removed

--- emWin_Wmal/emWin_Wmal.h
+++ emWin_Wmal/emWin_Wmal.h
@@ -24,40 +24,40 @@ private:
 public:
   STemWinWindowManager();
   virtual void Init(int32_t width, int32_t height);
-  virtual int32_t CreateWin(int32_t x, int32_t y, int32_t width, int32_t height);
-  virtual int32_t CreateText(int32_t parent, int32_t x, int32_t y, int32_t width, int32_t height, const char *text);
+  virtual int32_t CreateWin(int32_t x, int32_t y, int32_t width, int32_t height) override;
+  virtual int32_t CreateText(int32_t parent, int32_t x, int32_t y, int32_t width, int32_t height, const char *text) override;
   virtual int32_t CreateButton(
       int32_t parent, int32_t x, int32_t y,
       int32_t width, int32_t height, const char *text,
-      IButtonEventHandler *buttonEventHandler);
+      IButtonEventHandler *buttonEventHandler) override;
   virtual int32_t CreateListView(int32_t parent, int32_t x, int32_t y,
-      int32_t width, int32_t height);
+      int32_t width, int32_t height) override;
   virtual void AddColumnToListView(int32_t listViewHandle,
-      int32_t size, const char *name);
-  virtual void AddRowToListView(int32_t listViewHandle, const char **row);
-  virtual int32_t GetWidth();
-  virtual int32_t GetHeight();
-  virtual void Touch(int32_t x, int32_t y);
-  virtual void Untouch();
-  virtual void Show(int32_t windowHandle);
-  virtual void Hide(int32_t windowHandle);
+      int32_t size, const char *name) override;
+  virtual void AddRowToListView(int32_t listViewHandle, const char **row) override;
+  virtual int32_t GetWidth() override;
+  virtual int32_t GetHeight() override;
+  virtual void Touch(int32_t x, int32_t y) override;
+  virtual void Untouch() override;
+  virtual void Show(int32_t windowHandle) override;
+  virtual void Hide(int32_t windowHandle) override;
 
 
-  virtual void GetListViewText(int32_t listViewHandle, int32_t row, int32_t column, char *text, int32_t bufferSize){ };
-  virtual int32_t CreateEdit(int32_t hParent, int32_t x, int32_t y, int32_t width, int32_t height, int32_t textLength, const char *text){ };
-  virtual void SetEditText(int32_t editHandle, const char *text){ };
-  virtual void GetEditText(int32_t editHandle, char *text, int32_t maxLength){ };
-  virtual void SetTextBkColor(int32_t textHandle, uint32_t color){ };
-  virtual int32_t CreateComboBox(int32_t hParent, int32_t x, int32_t y, int32_t width, int32_t height, const char **items, const int32_t itemsCount){ };
-  virtual int32_t GetComboBoxSelection(int32_t comboBoxHandle){ };
-  virtual void SetComboBoxSelection(int32_t comboBoxHandle, int32_t selection){ };
-  virtual void DeleteListViewRows(int32_t listViewHandle){ };
-  virtual int32_t GetSelectedRow(int32_t listViewHandle){ };
+  virtual void GetListViewText(int32_t listViewHandle, int32_t row, int32_t column, char *text, int32_t bufferSize) override { };
+  virtual int32_t CreateEdit(int32_t hParent, int32_t x, int32_t y, int32_t width, int32_t height, int32_t textLength, const char *text)override { return 0; };
+  virtual void SetEditText(int32_t editHandle, const char *text)override{ };
+  virtual void GetEditText(int32_t editHandle, char *text, int32_t maxLength)override{ };
+  virtual void SetTextBkColor(int32_t textHandle, uint32_t color)override{ };
+  virtual int32_t CreateComboBox(int32_t hParent, int32_t x, int32_t y, int32_t width, int32_t height, const char **items, const int32_t itemsCount)override{ return 0; };
+  virtual int32_t GetComboBoxSelection(int32_t comboBoxHandle)override{ return 0; };
+  virtual void SetComboBoxSelection(int32_t comboBoxHandle, int32_t selection)override{ };
+  virtual void DeleteListViewRows(int32_t listViewHandle)override{ };
+  virtual int32_t GetSelectedRow(int32_t listViewHandle)override{ return 0; };
 
 
 
 
-  virtual bool Execute();
+  virtual bool Execute()override;
 };
 
 #endif
